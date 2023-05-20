c09cbe6e1a32c26d448b849e2e443ca2410b2bdb
Anna Kreidina <kav99mail@gmail.com>
2023-05-21 00:36:07 +0300

    //

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import Header from 'components/Header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-// import HashLoader from 'react-spinners/HashLoader';
 
 const Layout = () => {
   return (
@@ -10,15 +9,7 @@ const Layout = () => {
         <Header />
       </header>
       <main>
-        <Suspense
-        // fallback={
-        //   <HashLoader
-        //     color="#ff4800"
-        //     size={70}
-        //     cssOverride={{ margin: '80px auto' }}
-        //   />
-        // }
-        >
+        <Suspense>
           <Outlet />
         </Suspense>
       </main>
